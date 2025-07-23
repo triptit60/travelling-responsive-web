4b61232eef1f1467774be4174badb54c8bd70ad0
Tripti Tiwari <triptitiwari625@gmail.com>
2025-07-23 23:59:31 +0545

    scroll

--- js/main.js
+++ js/main.js
@@ -77,3 +77,36 @@ const swiperTestimonial = new Swiper('.testimonial__swiper', {
     disableOnIntraction: false,
   }
 })
+
+// ====================SHOW SCROLL UP========================
+const scrollUp = () =>{
+  const scrollUp =document.getElementById('scroll-up')
+  //when the scroll is higher than 350 viewport height, add the show-scroll class to the 
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                      : scrollUp.classList.remove("show-scroll")
+  
+}
+window.addEventListener('scroll', scrollUp)
+
+// ================SCROLL SECTIONS ACTIVE LINK====================================
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+  const scrollDown = window.scrollY
+  
+    sections.forEach(current =>{
+      const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+          sectionClass.classList.add('active-link')
+        }else{
+          sectionClass.classList.remove('active-link')
+        }
+
+    })
+  }
+
+window.addEventListener('scroll', scrollActive)
